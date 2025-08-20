18bb98200c1ee1ca72eb55cb469b2d8caf29332c
Daniil Shcherbakov <Charel@bk.ru>
2025-08-20 18:06:28 +0300

    CreateNoteScreen

--- app/src/main/java/ru/nvgsoft/mynotes/presentation/MainActivity.kt
+++ app/src/main/java/ru/nvgsoft/mynotes/presentation/MainActivity.kt
@@ -1,11 +1,10 @@
 package ru.nvgsoft.mynotes.presentation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import ru.nvgsoft.mynotes.presentation.screen.notes.NotesScreen
+import ru.nvgsoft.mynotes.presentation.screen.creation.CreateNoteScreen
 import ru.nvgsoft.mynotes.presentation.ui.theme.MyNotesTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,14 +13,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyNotesTheme {
-                    NotesScreen(
-                        onNoteClick = {
-                            Log.d("MainActivity", "onNoteClick: $it ")
-                        },
-                        onAddNoteClick = {
-                            Log.d("MainActivity", "onAddNoteClick")
-                        }
-                    )
+//                    NotesScreen(
+//                        onNoteClick = {
+//                            Log.d("MainActivity", "onNoteClick: $it ")
+//                        },
+//                        onAddNoteClick = {
+//                            Log.d("MainActivity", "onAddNoteClick")
+//                        }
+//                    )
+                CreateNoteScreen()
 
             }
         }
