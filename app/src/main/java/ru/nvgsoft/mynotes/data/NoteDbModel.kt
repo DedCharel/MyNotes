48d1c900cc19d9984a2d57b7dcea3360c7a2ec6d
Daniil Shcherbakov <Charel@bk.ru>
2025-08-24 11:06:57 +0300

    Create DAO

--- app/src/main/java/ru/nvgsoft/mynotes/data/NoteDbModel.kt
+++ app/src/main/java/ru/nvgsoft/mynotes/data/NoteDbModel.kt
@@ -3,7 +3,7 @@ package ru.nvgsoft.mynotes.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "notes")
 data class NoteDbModel(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
